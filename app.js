2952afc4a9400731b1e8d9b3a4c0094ee66cd5dd
mario <panggihmario@gmail.com>
2018-05-23 17:07:31 +0700

    crud customer

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 let app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended : false}))
+app.set('view engine','ejs')
 
 
 let index = require('./routes/index.js')
+let customer = require('./routes/customerRoutes.js')
 
 app.use('/',index)
+app.use('/customer',customer)
 
 
 app.listen(3000)
