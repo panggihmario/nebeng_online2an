68624346b2dde17d0d435f5c41dc973456ce5054
mario <panggihmario@gmail.com>
2018-05-24 19:36:42 +0700

    new

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
 const express = require('express')
 var router = express.Router()
+let model = require('../models')
 
 router.get('/',function(req,res){
-  res.send('home')
+  res.render('home.ejs')
 })
 
+router.get('/schedule',function(req,res){
+  model.Schedule.findAll()
+  .then(function(schedule){
+    // res.send(schedule)
+    res.render('schedule.ejs',{schedule : schedule})
+  })
+})
+
+router.get('/',function(req,res){
+  model.Schedule.findById(req.params.id)
+  .then(function(dataSchedule){
+    res.redirect('/',{dataSchedule : dataSchedule})
+  })
+})
+
+router.post('/',function(req,res){
+  model.Customer.Schedules.push(model.Schedule)
+  .then(function(){
+    res.redirect('/')
+  })
+})
 module.exports = router
--- routes/login.js
+++ routes/login.js
@@ -4,33 +4,43 @@ const router = express.Router();
 const model = require('../models');
 const bcrypt = require('bcrypt');
 
-router.get('/',function(req,res){
+router.get('/',function(req,res,next){
+    // res.render('./customer/loginCustomer.ejs' ,{messages : ""})
+    if(req.session.current_user){
+        res.send('login')
+    }else{
+        next()
+    }
+},function(req,res,next){
     res.render('./customer/loginCustomer.ejs' ,{messages : ""})
 })
 
-router.post('/',function(req,res){
-    let name = req.body.name
-    let password = req.body.password
+
+
+router.post('/',function(req,res,next){
+    var salt = bcrypt.genSaltSync(8);
+	var hash = bcrypt.hashSync(req.body.password,salt)
+    let password = bcrypt.compareSync(req.body.password,hash)
     model.Customer.findOne({
-        where : {name : name}
-    })
-    .then(function(customer){
-        if(customer){
-            var salt = bcrypt.genSaltSync(8);
-            var hash = bcrypt.hashSync(password,salt)
-            let checkPassword = bcrypt.compareSync(password,hash)
-            if(checkPassword){
-                req.session.customer = model.Customer.name
-                res.redirect('/')
-            }else{
-
-                res.render('./customer/loginCustomer.ejs',{messages : "Incorrect name or password "})
-            }
-        }else{
-
-            res.render('./customer/loginCustomer.ejs',{messages : "Incorrect name or password"})
+        where : {
+            name : req.body.name
         }
     })
+    .then(customer=>{
+        if(customer){
+            if(password){
+            req.session.current_user = customer
+            console.log("===========",req.session.current_user.dataValues)
+            next()
+            res.redirect('/schedule')
+        }else{
+            res.render('./customer/loginCustomer.ejs',{messages : "Incorrect name or password"})
+        }
+    }
+        
+    })
 })
+
+
 
 module.exports = router
